773c55cedec8141660702ea3f6a7ceb47759a6b9
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-05-03 18:46:10 +0200

    class triple and node are in place

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# turists_trap
-the third of challenges given to us by gwizdałła 
+# tourists_trap
+
+the third of challenges given to us by gwizdałła
+
+## first thing
+
+all the routes contain the incorrect number of passengers,
+
+> for some weird reason I need to subtract one from all values on the connections
+
+this is because the bus needs one place for the driver or shit.
+
+## The question remains:
+
+If we keep the place for driver of the bus,
+
+do we need to keep one for the tour guide?
+
+okay, so we subtract one from all the vertices,
+
+> next we multiply them by -1, so we have opposites of them,
+
+> that's because we want to have maximal spanning tree, but algorithms are for minimal, so we make data suite our methods
+
+> next we sort the data by number of the passengers
+
+> then we create trees out of sorted data 
+> this is the difficult part, because 
--- main.cpp
+++ main.cpp
@@ -2,10 +2,70 @@
 // Created by pc on 03.05.2021.
 //
 #include <iostream>
-int main(){
 
-    std::cout<<"it's a trap\n";
+struct triplet {
+    triplet(int first, int second, int cost) : first(first), second(second), cost(cost) {}
 
+    bool operator==(const triplet &rhs) const {
+        return cost == rhs.cost;
+    }
+
+    bool operator!=(const triplet &rhs) const {
+        return !(rhs == *this);
+    }
+
+    bool operator<(const triplet &rhs) const {
+        return cost < rhs.cost;
+    }
+
+    bool operator>(const triplet &rhs) const {
+        return rhs.cost < cost;
+    }
+
+    bool operator<=(const triplet &rhs) const {
+        return !(rhs < *this);
+    }
+
+    bool operator>=(const triplet &rhs) const {
+        return !(*this < rhs);
+    }
+
+
+    int first;
+    int second;
+    int cost;
+
+};
+
+class node {
+public:
+    node() {
+        value = 0;
+        left_ptr = nullptr;
+        left_cost = 0;
+        right_ptr = nullptr;
+        right_cost = 0;
+    }
+
+    node(int val, int left_val, int right_val) {
+        value = val;
+        left_ptr = nullptr;
+        left_cost = 0;
+        right_ptr = nullptr;
+        right_cost = 0;
+    }
+
+private:
+    int value;
+    node *left_ptr;
+    int left_cost;
+    node *right_ptr;
+    int right_cost;
+};
+
+int main() {
+
+    std::cout << "it's a trap\n";
 
 
     return 0;
